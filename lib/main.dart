7dfb66ce8745a5e1b426d6a6bcebf7f5693d99c0
Abraham3 <olumodejiabraham3@gmail.com>
2025-06-04 18:02:18 +0100

    fixed the initial route back to landing screen

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ class MyApp extends StatelessWidget {
 
             // Use onGenerateRoute instead of routes:
             onGenerateRoute: AppRoutes.generateRoute,
-            initialRoute: AppRoutes.viewDrug,
+            initialRoute: AppRoutes.landing,
           );
         },
       ),
